6897aa997a615e5f561a510c6d6888146c9cdb72
PieceOfEon <danilskripka1996@gmail.com>
2023-02-22 15:45:51 +0200

    end

--- ExSQLiteShop/MainWindow.xaml.cs
+++ ExSQLiteShop/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,13 +26,140 @@ namespace ExSQLiteShop
         {
             InitializeComponent();
         }
-
+        int kol = 0;
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             AdminWindow adminWindow = new AdminWindow();
             Visibility = Visibility.Hidden;
             adminWindow.ShowDialog();
             Visibility = Visibility.Visible;
+        }
+
+        private void Proverka()
+        {
+            try
+            {
+                if (UsernameH.Text != "" && PassH.Password.ToString() != "")
+                {
+                    string sqlExpression = "SELECT * FROM UserReg";
+                    using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                    {
+                        connection.Open();
+                        using (SqliteCommand command = new SqliteCommand(sqlExpression, connection))
+                        {
+                            SqliteDataReader reader = command.ExecuteReader();
+                            if (reader.HasRows)
+                            {
+                                string s1 = reader.GetName(1);
+                                //string s2 = reader.GetName(2);
+
+                                while (reader.Read())
+                                {
+                                    object nik = reader.GetValue(1);
+                                    //object pass = reader.GetValue(2);
+                                    if (UsernameH.Text == nik.ToString())
+                                    {
+                                        kol = 0;
+                                        MessageBox.Show("Этот ник занят. Попробуйте другой.");
+                                    }
+                                    else
+                                    {
+                                        kol = 1;
+                                    }
+
+                                }
+
+                                //int rowsAffected = command.ExecuteNonQuery();
+                                //InfSearch.Text = "Successfully";
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Proverka();
+                if(kol==1)
+                {
+                    if (UsernameH.Text != "" && PassH.Password.ToString() != "")
+                    {
+                        string str = "insert into UserReg (Login, Password) values ('" + UsernameH.Text + "', '" + PassH.Password.ToString() + "')"; ;
+                        using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                        {
+                            connection.Open();
+                            using (SqliteCommand command = new SqliteCommand(str, connection))
+                            {
+
+
+                                int rowsAffected = command.ExecuteNonQuery();
+                                //InfSearch.Text = "Successfully";
+                                MessageBox.Show($"Successfully {UsernameH.Text} reg.");
+
+                            }
+                        }
+                    }
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void ProverkaSignIn()
+        {
+            try
+            {
+                if (UsernameH.Text != "" && PassH.Password.ToString() != "")
+                {
+                    string sqlExpression = "SELECT * FROM UserReg";
+                    using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                    {
+                        connection.Open();
+                        using (SqliteCommand command = new SqliteCommand(sqlExpression, connection))
+                        {
+                            SqliteDataReader reader = command.ExecuteReader();
+                            if (reader.HasRows)
+                            {
+                                //string s1 = reader.GetName(1);
+                                //string s2 = reader.GetName(2);
+
+                                while (reader.Read())
+                                {
+                                    object nik = reader.GetValue(1);
+                                    object pass = reader.GetValue(2);
+                                    if (UsernameH.Text == nik.ToString() && PassH.Password.ToString()==pass.ToString())
+                                    {
+                                        MessageBox.Show("Successfully");
+                                        AdminWindow adminWindow = new AdminWindow();
+                                        adminWindow.ShowDialog();
+                                        break;
+                                    }
+                                 
+
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ProverkaSignIn();
         }
     }
 }
--- ExSQLiteShop/SellWindow.xaml.cs
+++ ExSQLiteShop/SellWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ControlzEx.Standard;
+using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +25,184 @@ namespace ExSQLiteShop
         public SellWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PrintBaza.Text = "";
+                string str = "select * from Books";
+                using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                {
+                    connection.Open();
+                    using (SqliteCommand command = new SqliteCommand(str, connection))
+                    {
+                        //command.Connection = connection;
+                        SqliteDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            //MessageBox.Show(s2);
+                            object id = reader.GetValue(0);
+                            object id1 = reader.GetValue(1);
+                            object id2 = reader.GetValue(2);
+                            object id3 = reader.GetValue(3);
+                            object id4 = reader.GetValue(4);
+                            object id5 = reader.GetValue(5);
+                            object id6 = reader.GetValue(6);
+                            object id7 = reader.GetValue(7);
+
+                            PrintBaza.Text += id + "\t" + id1 + "\t" + id2 + "\t" + id3 + "\t" + id4 + "\t" + id5 + "\t" + id6 + "\t" + id7 + "\n";
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (IDSell.Text != "")
+                {
+                    using (var connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                    {
+                        connection.Open();
+
+                        SqliteCommand command = new SqliteCommand("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db");
+                        command.Connection = connection;
+                        //Доабвление значений
+                        command.CommandText = "INSERT INTO Sells(idBooks) VALUES(" + IDSell.Text + ")";
+                  
+                        int number = command.ExecuteNonQuery();
+                        MessageBox.Show("Successfully");
+
+                    }
+                    Console.Read();
+
+                }
+            }
+            catch (Exception b) { MessageBox.Show(b.Message); };
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PrintSell.Text = "";
+                string str = "select * from Books,Sells where Books.id=Sells.idBooks";
+                using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                {
+                    connection.Open();
+                    using (SqliteCommand command = new SqliteCommand(str, connection))
+                    {
+                        //command.Connection = connection;
+                        SqliteDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            //MessageBox.Show(s2);
+                            object id = reader.GetValue(0);
+                            object id1 = reader.GetValue(1);
+                            object id2 = reader.GetValue(2);
+                            object id3 = reader.GetValue(3);
+                            object id4 = reader.GetValue(4);
+                            object id5 = reader.GetValue(5);
+                            object id6 = reader.GetValue(6);
+                            object id7 = reader.GetValue(7);
+
+                            PrintSell.Text += id + "\t" + id1 + "\t" + id2 + "\t" + id3 + "\t" + id4 + "\t" + id5 + "\t" + id6 + "\t" + id7 + "\n";
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PrintPopularSell.Text = "";
+                string str = "select * from Books,Sells\r\nwhere Books.id=Sells.idBooks\r\ngroup by Sells.idBooks\r\nhaving count(Books.id) =(select max(count_) as ma from(SELECT count(*) as count_ FROM Sells GROUP BY idBooks))";
+                using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                {
+                    connection.Open();
+                    using (SqliteCommand command = new SqliteCommand(str, connection))
+                    {
+                        //command.Connection = connection;
+                        SqliteDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            //MessageBox.Show(s2);
+                            object id = reader.GetValue(0);
+                            object id1 = reader.GetValue(1);
+                            object id2 = reader.GetValue(2);
+                            object id3 = reader.GetValue(3);
+                            object id4 = reader.GetValue(4);
+                            object id5 = reader.GetValue(5);
+                            object id6 = reader.GetValue(6);
+                            object id7 = reader.GetValue(7);
+
+                            PrintPopularSell.Text += id + "\t" + id1 + "\t" + id2 + "\t" + id3 + "\t" + id4 + "\t" + id5 + "\t" + id6 + "\t" + id7 + "\n";
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                PrintMaxPrice.Text = "";
+                string str = "select * from Books,Sells\r\nwhere Books.id=Sells.idBooks\r\ngroup by Books.Price\r\nhaving max(Books.Price) =(select max(Books.Price)from Books, Sells where Books.id=Sells.idBooks)";
+                using (SqliteConnection connection = new SqliteConnection("Data Source = D:\\ViStudio\\ExSQLiteShop\\ExSQLiteShop\\ExSQLiteShop.db"))
+                {
+                    connection.Open();
+                    using (SqliteCommand command = new SqliteCommand(str, connection))
+                    {
+                        //command.Connection = connection;
+                        SqliteDataReader reader = command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            //MessageBox.Show(s2);
+                            object id = reader.GetValue(0);
+                            object id1 = reader.GetValue(1);
+                            object id2 = reader.GetValue(2);
+                            object id3 = reader.GetValue(3);
+                            object id4 = reader.GetValue(4);
+                            object id5 = reader.GetValue(5);
+                            object id6 = reader.GetValue(6);
+                            object id7 = reader.GetValue(7);
+
+                            PrintMaxPrice.Text += id + "\t" + id1 + "\t" + id2 + "\t" + id3 + "\t" + id4 + "\t" + id5 + "\t" + id6 + "\t" + id7 + "\n";
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
